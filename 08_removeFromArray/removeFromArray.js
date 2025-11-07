f9bd50a908bb34d93887c4f1b7bfd935ce69b85e
Leo <liangweigain@gmail.com>
2025-11-07 15:43:17 +0800

    完成了6-9jsavascript练习问题
    对数组循环等方法有了更深的了解

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    const counterArr = [...arg];
+    const result = []
+    console.log(counterArr);
+    for (const toRemove of arr) {
+        if (!counterArr.includes(toRemove)) {
+            result.push(toRemove);
+        }
+    }
+    arr.length = 0;
+    console.log(result)
+    arr.push(...result);
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
+
