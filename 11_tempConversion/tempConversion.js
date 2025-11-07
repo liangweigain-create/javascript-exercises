b103bf3a83c68c8c413d00b5e63ccddfc6a8ed90
Leo <liangweigain@gmail.com>
2025-11-07 17:25:13 +0800

    FInished javascript excersice for this session(10-11)
    ready to do more

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+//fahrenheit = celsius * 1.8 + 32
+const convertToCelsius = function(fah) {
+  const result = (fah - 32) / 1.8
+  return Number.isInteger(result) ? result : Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const result = cel * 1.8 + 32
+  return Number.isInteger(result) ? result : Number(result.toFixed(1));
 };
 
 // Do not edit below this line
